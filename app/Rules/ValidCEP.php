9bda3920ac236b0954196df59c8cfb18104170ca
Júlia Mizarela <juliamizarela@id.uff.br>
2022-02-11 18:27:10 -0300

    ValidCEP follows Class naming convetion. Rule created.

--- app/Rules/ValidCEP.php
+++ app/Rules/ValidCEP.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class validCEP implements Rule
+class ValidCEP implements Rule
 {
     /**
      * Create a new rule instance.
@@ -23,9 +23,11 @@ class validCEP implements Rule
      * @param  mixed  $value
      * @return bool
      */
-    public function passes($attribute, $value)
+    public function passes($attribute, $cep)
     {
-        //
+        $xml = simplexml_load_string( @file_get_contents("http://viacep.com.br/ws/".$cep."/xml/"));
+        $erro = (string) $xml->erro;
+        return !$erro;
     }
 
     /**
